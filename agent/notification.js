10e4364245efab21557b62e012728933d3c4e043
Wooyoung Chung <yakle1218@gmail.com>
2015-01-07 21:20:04 -0800

    refactor apn function
    require(‘notification’).sendPush(list, user, msg) will push
    notification

--- agent/notification.js
+++ agent/notification.js
@@ -1,19 +1,23 @@
-var join = require('path').join
 
-var apn = require('apn');
-var options = {'pfx': join(__dirname, '../_cert/letUsPlayKey.p12'), 
-				'passphrase':'wjdgkdms1218'};
+// apn relate functionalities 
+module.exports = {
+	sendPush: function(deviceList, origin, message) {
+		var apn = require('apn');
+		var options = {'pfx': require('path').join(__dirname, '../_cert/letUsPlayKey.p12'),
+			'passphrase':'wjdgkdms1218'};
 
-var apnConnection = new apn.Connection(options);
-var deviceToken = require('../devices.js');
+		var apnConnection = new apn.Connection(options);
+		
+		for (var eachDevice in deviceList) {
+			var d = new apn.Device(eachDevice);
+			var push = new apn.Notification();
 
-var device = new apn.Device(deviceToken);
-var note = new apn.Notification();
+			push.expiry = Math.floor(Date.now() / 1000) + 3600;
+			push.sound = 'ping.aiff';
+			push.alert = origin + " is now playing " + message;
+			push.payload = {'message': message};
 
-note.expiry = Math.floor(Date.now() / 1000) + 3600;
-note.sound = 'ping.aiff';
-note.alert = '\uD83D\uDCE7 \u2709 You have a new message';
-note.payload = {'messageFrom': 'Me'};
-
-apnConnection.pushNotification(note, device);
-
+			appConnection.pushNotification(push, d);
+		}
+	}
+};
