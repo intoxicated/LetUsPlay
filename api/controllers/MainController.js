58e8416b33dbf07c46adddc7957ba7c744c90a01
Wooyoung Chung <yakle1218@gmail.com>
2015-01-13 16:09:06 -0800

    implemented register and authenticate function

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -4,24 +4,43 @@
 
 module.exports = {
     register: function(req, res) {
-        var email = req.body.email;
+        var nickname = req.body.nickname;
         var password = req.body.password;
         var dtoken = req.body.device_token;
         var platform = req.body.platform;
 
-        if(!email || !password)
-            return res.json(401, {error: "Missing email or password"});
+        if(!nickname || !password)
+            return res.json(401, {error: "missing nickname or password"});
 
-        User.create({email: email, password: password, device_token: dtoken, platform: platform}).exec(function(err, user) {
+        User.create({nickname: nickname, password: password, device_token: dtoken, platform: platform}).exec(function(err, user) {
             if(err)
                 return res.json(err.status, {error: "Error:" + err});
             if(user)
                 return res.json({user: user, token: UserManager.issueToken(user.id)});
+            else
+                return res.json(500, {error: "Unknwon error"});
         });
     },
 
     authenticate: function(req, res) {
+        var nickname = req.param("nickname");
+        var password = req.param("password");
 
+        User.find({nickname:nickname}).exec(function(err, user){
+            if(err)
+                return res.json(err.status, {error: "Error" + err});
+            if(!user)
+                return res.json(404, {error:"Error user not found"});
+
+            User.validPassword(password, user, function(err, value){
+                if(err)
+                    return res.json(403, {error: "Forbidden"});
+                if(!valid)
+                    return res.json(401, {error: "Invalid user nmae or password"});
+                else
+                    return res.json({user:nickname, token: UserManager.issueToken(user.id)});
+            });
+        });
     },
 
     logout: function(req, res) {
